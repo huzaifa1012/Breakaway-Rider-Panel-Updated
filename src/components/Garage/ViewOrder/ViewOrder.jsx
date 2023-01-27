da94be2001b8ff58516add5acc7d13bf8f70aa5e
huzaifahux <www.pakteach@gmail.com>
2023-01-27 19:44:03 +0500

    UI completed

--- src/components/Garage/ViewOrder/ViewOrder.jsx
+++ src/components/Garage/ViewOrder/ViewOrder.jsx
@@ -18,25 +18,64 @@ export default function ViewOrder() {
       <ResponsiveAppBar />
       <div className="view-order-wrap">
         <div className="view-order-main">
-
           <div className="view-top">
             <p className="view-card-heading">Customer Details</p>
-            <hr style={{marginTop:"0px"}} />
-            <h5><span className="property-of-view-Card"> Name : </span>  <span className="prop-value-of-view-card"> {data.customerName} </span> </h5>
-            <h5><span className="property-of-view-Card"> Phone : </span><span className="prop-value-of-view-card"> {data.customerPhone} </span> </h5>
-            <h5><span className="property-of-view-Card"> Email &nbsp; : </span> <span className="prop-value-of-view-card"> {data.customerEmail} </span> </h5>
-            <h5><span className="property-of-view-Card"> PayIn &nbsp; :</span> <span className="prop-value-of-view-card"> {data.customerPayment} </span> </h5>
+            <hr style={{ marginTop: "0px" }} />
+            <h5>
+              <span className="property-of-view-Card"> Name : </span>
+              <span className="prop-value-of-view-card">
+                
+                {data.customerName}
+              </span>
+            </h5>
+            <h5>
+              <span className="property-of-view-Card"> Phone : </span>
+              <span className="prop-value-of-view-card">
+                
+                {data.customerPhone}
+              </span>
+            </h5>
+            <h5>
+              <span className="property-of-view-Card"> Email &nbsp; : </span>
+              <span className="prop-value-of-view-card">
+                
+                {data.customerEmail}
+              </span>
+            </h5>
+            <h5>
+              <span className="property-of-view-Card"> CRCY &nbsp; :</span>
+              <span className="prop-value-of-view-card">
+                
+                {data.customerPayment}
+              </span>
+            </h5>
+          </div>
+          
+          <div className="view-bottom">
+            <p className="view-card-heading">Address</p>
+            <hr style={{ marginTop: "0px" }} />
+            <h5>
+              <span className="property-of-view-Card">Pick From : </span>
+              <span className="prop-value-of-view-card">
+                
+                {data.customerPickLocation}
+              </span>
+            </h5>
+            <h5>
+              <span className="property-of-view-Card">Drop Here : </span>
+              <span className="prop-value-of-view-card">
+                
+                {data.customerDropLocation}
+              </span>
+            </h5>
             <hr />
           </div>
-          <br />
-          <div className="view-bottom">
-          <p className="view-card-heading">Address</p>
-            <hr style={{marginTop:"0px"}} />
-          <h5>Pick From: {data.customerPickLocation}</h5>
-          <h5>Drop Here: {data.customerDropLocation}</h5>
-          <button onClick={SuccessFullDelivered}>Delivered</button>
-          <hr />
-        </div>
+
+            <div className="view-card-bottom">
+              <button className="Delivered_BTN" onClick={SuccessFullDelivered}>
+                Delivered
+              </button>
+            </div>
         </div>
       </div>
     </>
